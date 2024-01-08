90dca203877184805b0a05bf2b292bb24ac9b61e
Mark Nguyễn <manh9403@gmail.com>
2024-01-08 23:15:53 +0700

    a

--- Assets/Game/Script/PlayerMove.cs
+++ Assets/Game/Script/PlayerMove.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Rigidbody2D _rb;
+
+    private Transform _thisTransform;
+    private Vector2 _startPosition;
+    private Vector2 _endPosition;
+    private float _speed = 0.01f;
     void Start()
     {
-        
+        _thisTransform = this.transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rb.isKinematic = true;
+            _startPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _endPosition = Input.mousePosition;
+            Vector2 _pullDirection = _endPosition - _startPosition;
+            _rb.isKinematic = false;
+            _rb.AddForce(-_pullDirection * _speed, ForceMode2D.Impulse);
+        }
     }
 }
